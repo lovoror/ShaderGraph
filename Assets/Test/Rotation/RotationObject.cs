bcc2803ed9198af22abf31b778ec24e558296b6b
jacob.zhang <jacob.zhang@happyelements.com>
2021-05-17 19:43:15 +0800

    ++roration

---
+++ Assets/Test/Rotation/RotationObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationObject : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // speed += Time.deltaTime * 10;
+        // transform.rotation = Quaternion.Euler(0,speed,0);
+        transform.Rotate(Vector3.up * (Time.deltaTime * speed));
+    }
+}
